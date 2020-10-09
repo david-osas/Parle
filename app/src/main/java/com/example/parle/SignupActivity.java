ad904745652c6cf8a85cdd1abb58d5d83845de37
david-osas <azamegbeosarumense@gmail.com>
2020-10-09 23:24:26 +0100

    Add Sign up UI

--- app/src/main/java/com/example/parle/SignupActivity.java
+++ app/src/main/java/com/example/parle/SignupActivity.java
@@ -1,26 +1,21 @@
 package com.example.parle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
-    private LoginViewModel viewModel;
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-
+        setContentView(R.layout.activity_signup);
     }
 
     public void toggle(View view){
-        Intent intent = new Intent(this, SignupActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
