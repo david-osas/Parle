f3b714562fb22a75923b07bc4fe1977f5fe7c919
Ose4g <osemudiamenitua@gmail.com>
2020-10-23 06:19:37 +0100

    working on the details activity viewmodel

--- app/src/main/java/com/example/parle/DetailsActivity/DetailsActivity.java
+++ app/src/main/java/com/example/parle/DetailsActivity/DetailsActivity.java
@@ -1,4 +1,4 @@
-package com.example.parle;
+package com.example.parle.DetailsActivity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +15,16 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.parle.ConcentrateActivity;
 import com.example.parle.Models.Student;
+import com.example.parle.R;
 import com.example.parle.databinding.ActivityDetailsBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
---
+++ app/src/main/java/com/example/parle/DetailsActivity/DetailsActivityViewModel.java
@@ -0,0 +1,7 @@
+package com.example.parle.DetailsActivity;
+
+import androidx.lifecycle.ViewModel;
+
+public class DetailsActivityViewModel extends ViewModel {
+
+}
